810a04a2d575a12af0d751f2dc22c524bf75ef6a
carolx <carolx@berkeley.edu>
2023-08-21 22:45:05 -0400

    verification is done

--- logic/verify_attestation.js
+++ logic/verify_attestation.js
@@ -12,9 +12,10 @@ eas.connect(signer);
 
 async function verify_attestation(uid) {
     const attestation = await eas.getAttestation(uid);
-    console.log(attestation);
+    console.log("Your attester is %s, recipient is %s, and data is %s", attestation.attester, attestation.recipient, attestation.data); 
+    return attestation; 
 }
 
-// test 
+// test uid 
 // uid = "0xff7ee5683636e3b9a10284f3b1ea9dfcc64bca759989018a06b52b314cdd8f97";
-verify_attestation(uid);
+attestation = verify_attestation(uid);
